3a847b728efeab3bd70b146ca897348f77000be0
abe <abel.rogers@gmail.com>
2023-08-09 11:26:15 +0100

    update cors

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -4,6 +4,11 @@ const axios = Axios.create({
     baseURL: process.env.NEXT_PUBLIC_BACKEND_URL,
     headers: {
         'X-Requested-With': 'XMLHttpRequest',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods':
+            'GET,PUT,POST,DELETE,PATCH,OPTIONS,HEAD',
+        'Access-Control-Allow-Headers': '* ',
     },
     withCredentials: true,
 })
